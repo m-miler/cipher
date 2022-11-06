dc5ec2221895112b162e6d316ed34f551a2086bb
Mateusz <mmiler30@gmail.com>
2022-11-06 12:27:18 +0100

    feat: add show_rot_submenu function

--- cipher_app/menu.py
+++ cipher_app/menu.py
@@ -5,14 +5,22 @@ class Menu:
     @staticmethod
     def show() -> None:
         print("Menu:")
-        print("1. Encrypt message (ROT13, ROT47)")
-        print("2. Decrypt message (ROT13, ROT47)")
+        print("1. Encrypt message")
+        print("2. Decrypt message")
         print("3. Save encrypted messages to the chosen file")
-        print("4. Read and decrypt text from file")
-        print("5. Exit")
+        print("4. Read and decrypt message from file")
+        print("5. Exit\n")
 
     @staticmethod
     def get_user_choice() -> Any:
         user_input = input('Please select number from menu to execute --> ')
         return user_input
 
+    @staticmethod
+    def show_rot_submenu() -> Any:
+        """ Function will show the menu on the screen to get user cipher method choose """
+        print("Cipher list:")
+        print("1. ROT13")
+        print("2. ROT47")
+        cipher_type = input("Please choose the cipher method: ")
+        return cipher_type
