7622acd5fec334b8281e7406ef452933673bc2e6
Mateusz <mmiler30@gmail.com>
2022-11-06 21:20:57 +0100

    docs: add \n space to clear prints

--- cipher_app/menu.py
+++ cipher_app/menu.py
@@ -9,18 +9,19 @@ class Menu:
         print("2. Decrypt message")
         print("3. Save encrypted messages to the chosen file")
         print("4. Read and decrypt message from file")
-        print("5. Exit\n")
+        print("5. Exit")
 
     @staticmethod
     def get_user_choice() -> Any:
-        user_input = input('Please select number from menu to execute --> ')
+        user_input = input('\nPlease select number from menu to execute --> ')
         return user_input
 
     @staticmethod
     def show_rot_submenu() -> Any:
         """ Function will show the menu on the screen to get user cipher method choose """
-        print("Cipher list:")
+        print("\nCipher list:")
         print("1. ROT13")
         print("2. ROT47")
-        cipher_type = input("Please choose the cipher method: ")
+        print("3. Main Manu")
+        cipher_type = input("\nPlease choose the cipher method: ")
         return cipher_type
