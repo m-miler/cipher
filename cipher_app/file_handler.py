d30152b98e5b91aa4591f9df4c73ccc3bb5e558a
Mateusz <mmiler30@gmail.com>
2022-11-07 22:34:53 +0100

    fix: change os to Path library

--- cipher_app/file_handler.py
+++ cipher_app/file_handler.py
@@ -1,14 +1,15 @@
 from cipher_app.buffer import Buffer
-import os
+from pathlib import Path
 
 
 class FileHandler:
     @staticmethod
     def write_to_file(path: str, messages: Buffer) -> None:
-        path = fr'{path}'
+        path = Path(path)
         with open(path, 'a') as file:
             for msg in messages.buffer:
                 file.writelines(msg + '\n')
+            file.close()
 
     @staticmethod
     def read_from_file(path: str) -> str:
