88d3d426234f61aa23174d1e9b24d36966872670
Mateusz <mmiler30@gmail.com>
2022-11-07 21:40:34 +0100

    feat: add new method save_to_the_file in a FileHandler class

--- cipher_app/file_handler.py
+++ cipher_app/file_handler.py
@@ -1,7 +1,14 @@
+from cipher_app.buffer import Buffer
+import os
+
+
 class FileHandler:
     @staticmethod
-    def write_to_file(path: str) -> None:
-        pass
+    def write_to_file(path: str, messages: Buffer) -> None:
+        path = fr'{path}'
+        with open(path, 'a') as file:
+            for msg in messages.buffer:
+                file.writelines(msg + '\n')
 
     @staticmethod
     def read_from_file(path: str) -> str:
