6194163e368b19cad2cb1754d7a7dcbe0711094a
Mateusz <mmiler30@gmail.com>
2022-11-09 13:40:25 +0100

    test(fix): fix write_to_file function test, add capture test to check the output message

--- cipher_app/tests/test_file_handler.py
+++ cipher_app/tests/test_file_handler.py
@@ -3,12 +3,14 @@ from cipher_app.buffer import Buffer
 import pytest
 
 
-def test_save_to_file_should_create_new_file_and_write_text(tmpdir):
+def test_save_to_file_should_create_new_file_and_write_text(tmpdir, capsys):
     file = tmpdir.join('text.txt')
     buffer = Buffer()
     buffer.add('Test Tekst 1')
     FileHandler.write_to_file(file.strpath, buffer)
+    capture = capsys.readouterr()
     assert file.read() == 'Test Tekst 1\n'
+    assert capture.out.strip() == 'All messages have been saved!'
 
 
 def test_read_from_file_should_return_list_with_all_lines_from_file(tmpdir):
