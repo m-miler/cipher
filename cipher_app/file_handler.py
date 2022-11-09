7cbbb4c4718c309c23ede14b80e5edcc24a40ab8
Mateusz <mmiler30@gmail.com>
2022-11-09 13:37:41 +0100

    feat: add read_from_file function

--- cipher_app/file_handler.py
+++ cipher_app/file_handler.py
@@ -6,12 +6,20 @@ class FileHandler:
     @staticmethod
     def write_to_file(path: str, messages: Buffer) -> None:
         path = Path(path)
-        with open(path, 'a') as file:
+        with open(path, 'a', encoding="utf-8") as file:
             for msg in messages.buffer:
                 file.writelines(msg + '\n')
             file.close()
             print('All messages have been saved!')
 
     @staticmethod
-    def read_from_file(path: str) -> str:
-        pass
+    def read_from_file(path: str) -> list:
+        path = Path(path)
+        with open(path, 'r', encoding="utf-8") as file:
+            lines = file.readlines()
+        test_to_decrypt = [line.strip() for line in lines]
+        return test_to_decrypt
+
+
+if __name__ == "__main__":
+    print(FileHandler.read_from_file('test.txt'))
