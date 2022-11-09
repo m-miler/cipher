eb92195324f879e72aca67daa9e2ca60ee764947
Mateusz <mmiler30@gmail.com>
2022-11-09 16:06:17 +0100

    fix: add new __get_cipher_type method which validate te correct input from user. Also refactor rest of the methods.

--- cipher_app/manager.py
+++ cipher_app/manager.py
@@ -1,7 +1,7 @@
 from cipher_app.buffer import Buffer
+from cipher_app.file_handler import FileHandler
 from cipher_app.menu import Menu
 from cipher_app.rot import ROT13, ROT47
-from cipher_app.file_handler import FileHandler
 
 
 class Manager:
@@ -12,6 +12,7 @@ class Manager:
             "1": self.__encrypt_message,
             "2": self.__decrypt_message,
             "3": self.__save_to_the_file,
+            "4": self.__decrypt_from_file,
             "5": self.__exit_app
         }
         self.__rot_options = {
@@ -35,32 +36,35 @@ class Manager:
         else:
             print("Incorrect option")
 
+    def __get_cipher_type(self) -> str:
+        """ Method for get user input for the cipher type and validate
+            if user put correct option"""
+        cipher_type: str = Menu.show_rot_submenu()
+        if cipher_type in self.__rot_options:
+            return cipher_type
+        else:
+            print('Incorrect option')
+
     def __encrypt_message(self) -> None:
         """ Method for encrypt user input message and save it to the buffer list than
             print out the success message with encrypted text """
-        cipher_type: str = Menu.show_rot_submenu()
-        if cipher_type in self.__rot_options:
+        cipher_type: str = self.__get_cipher_type()
+        if cipher_type:
             text_to_encrypt: str = input('Please write message to encrypt: ')
             encrypt_messages: str = self.__rot_options.get(cipher_type).encrypt(text_to_encrypt)
             self.buffer.add(encrypt_messages)
             print(f'Message has been encrypted! -- {encrypt_messages}')
-        elif cipher_type == str(len(self.__rot_options) + 1):
-            return
         else:
-            print('Incorrect option')
             self.__encrypt_message()
 
     def __decrypt_message(self) -> None:
         """ Method for decrypt user input message and print out the success message with decrypted text """
-        cipher_type: str = Menu.show_rot_submenu()
-        if cipher_type in self.__rot_options:
+        cipher_type: str = self.__get_cipher_type()
+        if cipher_type:
             text_to_decrypt: str = input('Please write message to decrypt: ')
             decrypt_messages: str = self.__rot_options.get(cipher_type).decrypt(text_to_decrypt)
             print(f'Message has been decrypted! -- {decrypt_messages}')
-        elif cipher_type == str(len(self.__rot_options) + 1):
-            return
         else:
-            print('Incorrect option')
             self.__decrypt_message()
 
     def __save_to_the_file(self) -> None:
