bfd10ae8fa9010ffbeb98639f596074143c1a80c
Mateusz <mmiler30@gmail.com>
2022-11-09 13:36:29 +0100

    test: add read_from_file test

--- cipher_app/tests/test_file_handler.py
+++ cipher_app/tests/test_file_handler.py
@@ -10,3 +10,14 @@ def test_save_to_file_should_create_new_file_and_write_text(tmpdir):
     FileHandler.write_to_file(file.strpath, buffer)
     assert file.read() == 'Test Tekst 1\n'
 
+
+def test_read_from_file_should_return_list_with_all_lines_from_file(tmpdir):
+    file = tmpdir.join('test.txt')
+    with open(file.strpath, 'w', encoding="utf-8") as f:
+        for i in range(4):
+            f.writelines('Eóuće ńa ćbckfońhoźśk\n')
+    text_to_decrypt = FileHandler.read_from_file(file.strpath)
+    assert text_to_decrypt == ['Eóuće ńa ćbckfońhoźśk',
+                               'Eóuće ńa ćbckfońhoźśk',
+                               'Eóuće ńa ćbckfońhoźśk',
+                               'Eóuće ńa ćbckfońhoźśk']
